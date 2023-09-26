b0146649120a5a1f54ac3fde6b16a313b9ebcbfb
k22037403 <k22037403@kcl.ac.uk>
2023-09-26 19:29:10 +0100

    initial commit

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,15 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
+            
+    print(frequencies)
     return frequencies
+
+i = [100, 'Hello', '100', '100', 100]
+frequencies(i)
